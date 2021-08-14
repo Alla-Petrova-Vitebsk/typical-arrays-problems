94d424d6536bcc4c7bde3af02c51d03a8d7fd462
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2021-08-14 15:49:56 +0300

    feat: task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
+    let minval = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (minval > array[i]) {
+            minval = array[i]
+        }
+    }
+    return minval;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let maxval = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (maxval < array[i]) {
+            maxval = array[i]
+        }
+    }
+    return maxval;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) return 0;
+
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return (sum / array.length);
 }
